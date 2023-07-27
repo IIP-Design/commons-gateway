deac87e061fe69d1519bfd9f48df2d73f00853d9
mrewers <hello@marek.dev>
2023-07-27 16:45:54 -0400

    docs: document hashing functions for greater clarity

--- creds/hashing.go
+++ creds/hashing.go
@@ -8,17 +8,25 @@ import (
 	"golang.org/x/crypto/pbkdf2"
 )
 
-func GeneratePassword() (string, string) {
+// generateOTP creates a random 20 character string to be used as a password
+// as well as a random 10 character string to salt the password when hashing
+// it for storage in the database.
+func generateOTP() (string, string) {
 	pass := randstr.String(20)
 	salt := randstr.String(10)
 
 	return pass, salt
 }
 
-func GenerateHash(pass string, salt string) string {
+// generateHash returns a base64-encoded hash of the provided password and salt values.
+// The salt is appended to the password and the combination is run through 4096 iterations
+// of PBKDF2 using the SHA-256 hashing function. The resulting 32 byte derived key is then
+// encoded as a base64 string for ease of use.
+func generateHash(pass string, salt string) string {
 	var iterations = 4096
+	var keyLength = 32
 
-	derivedKey := pbkdf2.Key([]byte(pass), []byte(salt), iterations, 32, sha256.New)
+	derivedKey := pbkdf2.Key([]byte(pass), []byte(salt), iterations, keyLength, sha256.New)
 
 	encoded := base64.StdEncoding.EncodeToString(derivedKey)
 
