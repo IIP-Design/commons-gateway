ca864d29f72d9e58647fc6f8da306f1935ca3a97
mrewers <hello@marek.dev>
2023-08-01 15:48:23 -0400

    build: add table for admin data to the DB

--- .gateway-dev/reset/reset.sh
+++ .gateway-dev/reset/reset.sh
@@ -4,4 +4,6 @@ container=gateway_db
 database=gateway_dev
 user=gateway_dev
 
-docker exec -it $container psql -d $database -U $user -c 'TRUNCATE credentials, invites;'
+truncate_query='TRUNCATE admins, credentials, invites;'
+
+docker exec -it $container psql -d $database -U $user -c "$truncate_query"
