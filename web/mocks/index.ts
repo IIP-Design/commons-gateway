d51656ee89a945e59014339d76107ba74227a4a6
mrewers <hello@marek.dev>
2023-08-15 17:30:20 -0400

    test: add functions to quickly mock user and team data

---
+++ web/mocks/index.ts
@@ -0,0 +1,14 @@
+import { createMockUsers } from './users';
+import { createMockTeams } from './teams';
+
+/**
+ * Create a specified number of mock users and teams.
+ * @param userCount The number of users to generate.
+ * @param teamCount The number of teams to generate.
+ */
+export const mockSomeData = ( userCount: number, teamCount: number ) => {
+  const teams = createMockTeams(teamCount);
+  const users = createMockUsers(userCount, teamCount);
+
+  return {teams, users};
+}
---
+++ web/mocks/users.ts
@@ -0,0 +1,34 @@
+import { generateName, randomInt } from "./utils";
+
+/**
+ * Generates a mock users. For the purposes of the team id,
+ * five possible teams are assumed unless otherwise specified.
+ * @param teams The number of available teams.
+ */
+const createUser = (teams?: number): IUser => {
+  const lastName = generateName(10);
+  
+  return {
+    firstName: generateName(10),
+    lastName,
+    email: `${lastName.toLowerCase()}@test.com`,
+    team: (randomInt(teams || 5) + 1).toString(),
+    active: randomInt(2) === 0,
+  }
+};
+
+/**
+ * Generates a list of users of a specified length.
+ * @param count The number of teams to create.
+ * @param teams The number of available teams.
+ */
+export const createMockUsers = (count: number, teams?: number) => {
+  const mockUsers = [] as IUser[];
+
+  for (let i = 0; i < count; i++) {
+    const user = createUser(teams);
+    mockUsers.push(user)
+  }
+
+  return mockUsers;
+}
