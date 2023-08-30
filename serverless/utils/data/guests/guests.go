8d902f5dcc08f6df3f13172aece6b026cd10dcf2
mrewers <hello@marek.dev>
2023-08-30 10:28:37 -0400

    feat: add lambda to retrieve a single guest user

--- serverless/utils/data/guests/guests.go
+++ serverless/utils/data/guests/guests.go
@@ -1,31 +1,59 @@
 package guests
 
 import (
-	"fmt"
+	"database/sql"
+	"time"
 
 	"github.com/IIP-Design/commons-gateway/utils/data/data"
 	"github.com/IIP-Design/commons-gateway/utils/logs"
 )
+
+func RetrieveGuest(email string) (map[string]string, error) {
+	var guest map[string]string
+
+	pool := data.ConnectToDB()
+	defer pool.Close()
+
+	var firstName string
+	var lastName string
+	var team string
+	var expiration time.Time
+
+	query := `SELECT first_name, last_name, team, expiration FROM guests WHERE email = $1`
+	err := pool.QueryRow(query, email).Scan(&firstName, &lastName, &team, &expiration)
+
+	if err != nil {
+		logs.LogError(err, "Retrieve Guest Query Error")
+	}
+
+	guest = map[string]string{
+		"email":      email,
+		"givenName":  firstName,
+		"familyName": lastName,
+		"team":       team,
+		"expiration": expiration.String(),
+	}
+
+	return guest, err
+}
 
 // RetrieveGuests opens a database connection and retrieves the full list of admin users.
 func RetrieveGuests(team string) ([]map[string]string, error) {
 	var guests []map[string]string
 	var err error
 	var query string
+	var rows *sql.Rows
 
 	pool := data.ConnectToDB()
 	defer pool.Close()
 
 	if team == "" {
 		query = `SELECT email, first_name, last_name, team, expiration FROM guests`
+		rows, err = pool.Query(query)
 	} else {
-		query = fmt.Sprintf(
-			`SELECT email, first_name, last_name, team, expiration FROM guests WHERE team = '%s';`,
-			team,
-		)
+		query = `SELECT email, first_name, last_name, team, expiration FROM guests WHERE team = $1;`
+		rows, err = pool.Query(query, team)
 	}
-
-	rows, err := pool.Query(query)
 
 	if err != nil {
 		logs.LogError(err, "Get Guests Query Error")
