76bb77904f14a32144458b495f947ac4837b53cf
Ryan Quinn <ryan.quinn@stonewalldefense.org>
2023-09-06 12:30:13 -0400

    feat(web/src/components/UserTable): Example table with sorting and searching WIP

--- web/src/components/UserTable.tsx
+++ web/src/components/UserTable.tsx
@@ -1,15 +1,68 @@
-import { useEffect, useState } from 'react';
+// ////////////////////////////////////////////////////////////////////////////
+// React Imports
+// ////////////////////////////////////////////////////////////////////////////
+import { useEffect, useMemo, useState } from 'react';
 import type { FC } from 'react';
 
+// ////////////////////////////////////////////////////////////////////////////
+// 3PP Imports
+// ////////////////////////////////////////////////////////////////////////////
+import {
+  Column,
+  Table as ReactTable,
+  useReactTable,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  ColumnDef,
+  flexRender,
+  getSortedRowModel,
+  SortingState,
+} from '@tanstack/react-table'
+
+// ////////////////////////////////////////////////////////////////////////////
+// Local Imports
+// ////////////////////////////////////////////////////////////////////////////
 import { buildQuery } from '../utils/api';
 import currentUser from '../stores/current-user';
+import type { TUserRole } from '../stores/current-user';
 import { isGuestActive } from '../utils/guest';
 import { getTeamName } from '../utils/team';
 import { selectSlice } from '../utils/arrays';
 import { userIsSuperAdmin } from '../utils/auth';
 import { renderCountWidget, setIntermediatePagination } from '../utils/pagination';
 
+// ////////////////////////////////////////////////////////////////////////////
+// Styles and CSS
+// ////////////////////////////////////////////////////////////////////////////
 import style from '../styles/table.module.scss';
+
+// ////////////////////////////////////////////////////////////////////////////
+// Types and Interfaces
+// ////////////////////////////////////////////////////////////////////////////
+interface IUserEntry {
+  email: string;
+  expiration: string;
+  familyName: string;
+  givenName: string;
+  role: TUserRole;
+  team: string;
+}
+
+interface ITeam {
+  active: boolean;
+  id: string;
+  name: string;
+}
+
+interface ITableProps {
+  data: IUserEntry[];
+  columns: ColumnDef<IUserEntry>[];
+}
+
+// ////////////////////////////////////////////////////////////////////////////
+// Implementation
+// ////////////////////////////////////////////////////////////////////////////
 
 const UserTable: FC = () => {
   // Set the high and low ends of the view toggle.
@@ -18,9 +71,9 @@ const UserTable: FC = () => {
 
   const [viewCount, setViewCount] = useState( LOW_VIEW );
   const [viewOffset, setViewOffset] = useState( 0 );
-  const [userList, setUserList] = useState( selectSlice( [], viewCount, viewOffset ) );
+  const [userList, setUserList] = useState<IUserEntry[]>( selectSlice( [], viewCount, viewOffset ) );
   const [userCount, setUserCount] = useState( userList.length ); // eslint-disable-line no-unused-vars, @typescript-eslint/no-unused-vars
-  const [teams, setTeams] = useState( [] );
+  const [teams, setTeams] = useState<ITeam[]>( [] );
 
   useEffect( () => {
     const body = userIsSuperAdmin() ? {} : { team: currentUser.get().team };
@@ -176,4 +229,305 @@ const UserTable: FC = () => {
   );
 };
 
-export default UserTable;
+const PaginationFooter = <T, >( { table }: { table: ReactTable<T> } ) => {
+  return (
+    <div className="flex items-center gap-2">
+        <button
+          className="border rounded p-1"
+          onClick={() => table.setPageIndex(0)}
+          disabled={!table.getCanPreviousPage()}
+        >
+          {'<<'}
+        </button>
+        <button
+          className="border rounded p-1"
+          onClick={() => table.previousPage()}
+          disabled={!table.getCanPreviousPage()}
+        >
+          {'<'}
+        </button>
+        <button
+          className="border rounded p-1"
+          onClick={() => table.nextPage()}
+          disabled={!table.getCanNextPage()}
+        >
+          {'>'}
+        </button>
+        <button
+          className="border rounded p-1"
+          onClick={() => table.setPageIndex(table.getPageCount() - 1)}
+          disabled={!table.getCanNextPage()}
+        >
+          {'>>'}
+        </button>
+        <span className="flex items-center gap-1">
+          <div>Page</div>
+          <strong>
+            {table.getState().pagination.pageIndex + 1} of{' '}
+            {table.getPageCount()}
+          </strong>
+        </span>
+        <span className="flex items-center gap-1">
+          | Go to page:
+          <input
+            type="number"
+            defaultValue={table.getState().pagination.pageIndex + 1}
+            onChange={e => {
+              const page = e.target.value ? Number(e.target.value) - 1 : 0
+              table.setPageIndex(page)
+            }}
+            className="border p-1 rounded w-16"
+          />
+        </span>
+        <select
+          value={table.getState().pagination.pageSize}
+          onChange={e => {
+            table.setPageSize(Number(e.target.value))
+          }}
+        >
+          {[10, 20, 30, 40, 50].map(pageSize => (
+            <option key={pageSize} value={pageSize}>
+              Show {pageSize}
+            </option>
+          ))}
+        </select>
+      </div>
+  );
+}
+
+const Filter = ({
+  column,
+  table,
+}: {
+  column: Column<any, any>
+  table: ReactTable<any>
+} ) => {
+  const firstValue = table
+    .getPreFilteredRowModel()
+    .flatRows[0]?.getValue(column.id)
+
+  const columnFilterValue = column.getFilterValue()
+
+  return typeof firstValue === 'number' ? (
+    <div className="flex space-x-2">
+      <input
+        type="number"
+        value={(columnFilterValue as [number, number])?.[0] ?? ''}
+        onChange={e =>
+          column.setFilterValue((old: [number, number]) => [
+            e.target.value,
+            old?.[1],
+          ])
+        }
+        placeholder={`Min`}
+        className="w-24 border shadow rounded"
+      />
+      <input
+        type="number"
+        value={(columnFilterValue as [number, number])?.[1] ?? ''}
+        onChange={e =>
+          column.setFilterValue((old: [number, number]) => [
+            old?.[0],
+            e.target.value,
+          ])
+        }
+        placeholder={`Max`}
+        className="w-24 border shadow rounded"
+      />
+    </div>
+  ) : (
+    <input
+      type="text"
+      value={(columnFilterValue ?? '') as string}
+      onChange={e => column.setFilterValue(e.target.value)}
+      placeholder={`Search...`}
+      className="w-36 border shadow rounded"
+    />
+  )
+}
+
+const Table: FC<ITableProps> = ( { data, columns }: ITableProps ) => {
+  const [sorting, setSorting] = useState<SortingState>([]);
+
+  const table = useReactTable<IUserEntry>({
+    data,
+    columns,
+    state: {
+      sorting,
+    },
+
+    onSortingChange: setSorting,
+    getCoreRowModel: getCoreRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+  });
+
+  return (
+    <div className={ style.container }>
+      <table className={ `${style.table} ${style['user-table']}` }>
+        <thead>
+          {table.getHeaderGroups().map(headerGroup => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map(header => {
+                return (
+                  <th key={header.id} colSpan={header.colSpan}>
+                    {header.isPlaceholder ? null : (
+                      <div
+                      {...{
+                        className: header.column.getCanSort()
+                          ? 'cursor-pointer select-none'
+                          : '',
+                        onClick: header.column.getToggleSortingHandler(),
+                      }}
+                      >
+                        {flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                        {{
+                          asc: ' 🔺',
+                          desc: ' 🔻',
+                        }[header.column.getIsSorted() as string] ?? null}
+                        {header.column.getCanFilter() ? (
+                          <div>
+                            <Filter column={header.column} table={table} />
+                          </div>
+                        ) : null}
+                      </div>
+                    )}
+                  </th>
+                )
+              })}
+            </tr>
+          ))}
+        </thead>
+        <tbody>
+          {table.getRowModel().rows.map(row => {
+            return (
+              <tr key={row.id}>
+                {row.getVisibleCells().map(cell => {
+                  return (
+                    <td key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </td>
+                  )
+                })}
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+      { table.getPageCount() > 1 && <PaginationFooter table={table} /> }
+    </div>
+  );
+}
+
+function capitalize(str: string): string {
+  if( !str || !str.length ) {
+      return "";
+  }
+
+  const lower = str.toLowerCase();
+  return lower.substring( 0, 1 ).toUpperCase() + lower.substring( 1, lower.length );
+}
+
+const titleCase = ( str: string ) => {
+  const parts =
+        str
+            ?.replace(/([A-Z])+/g, capitalize)
+            // eslint-disable-next-line no-useless-escape
+            ?.split(/(?=[A-Z])|[\.\-\s_]/)
+            .map(x => x.toLowerCase()) ?? [];
+
+    if( parts.length === 0 ) {
+        return "";
+    }
+
+    parts[0] = capitalize(parts[0]);
+
+    return parts.reduce( ( acc, part ) => {
+        return `${acc} ${part.charAt(0).toUpperCase()}${part.slice(1)}`;
+    } );
+}
+
+const defaultColumnDef = <T, >( key: keyof T ): ColumnDef<T> => {
+  return {
+    accessorFn: row => row[key],
+    id: key as string,
+    cell: info => info.getValue(),
+    header: () => <span>{titleCase(key as string)}</span>,
+    footer: props => props.column.id,
+    enableSorting: true,
+  }
+};
+
+const UserTable2: FC = () => {
+  const [users, setUsers] = useState<IUserEntry[]>( [] );
+  const [teams, setTeams] = useState<ITeam[]>( [] );
+
+  useEffect( () => {
+    const body = userIsSuperAdmin() ? {} : { team: currentUser.get().team };
+
+    const getTeams = async () => {
+      const response = await buildQuery( 'teams', null, 'GET' );
+      const { data } = await response.json();
+
+      if ( data ) {
+        setTeams( data );
+      }
+    };
+
+    const getUsers = async () => {
+      const response = await buildQuery( 'guests', body );
+      const { data } = await response.json();
+
+      if ( data ) {
+        setUsers( data );
+      }
+    };
+
+    getTeams().then( () => getUsers() );
+  }, [] );
+
+  const columns = useMemo<ColumnDef<IUserEntry>[]>(
+    () => [
+      defaultColumnDef( 'givenName' ),
+      defaultColumnDef( 'familyName' ),
+      defaultColumnDef( 'email' ),
+      {
+        ...defaultColumnDef( 'team' ),
+        cell: info => getTeamName( info.getValue() as string, teams ),
+      },
+      {
+        ...defaultColumnDef( 'expiration' ),
+        cell: info => {
+          const exp = info.getValue() as string;
+          const isActive = isGuestActive( exp );
+          return (
+            <span className={ style.status }>
+              <span className={ isActive ? style.active : style.inactive } />
+              { isActive ? 'Active' : 'Inactive' }
+            </span>
+          );
+        },
+      },
+    ],
+    []
+  );
+
+  return (
+    <div className={ style.container }>
+      <Table
+        {
+          ...{ data: users, columns }
+        }
+      />
+    </div>
+  );
+}
+
+export default UserTable2;
