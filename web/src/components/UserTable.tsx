b9555e5adbe1589bb832a35ce978c600f3a6e15e
Ryan Quinn <ryan.quinn@stonewalldefense.org>
2023-09-07 13:14:04 -0400

    feat(web/src/components/Table): Adds full-featured user table

--- web/src/components/UserTable.tsx
+++ web/src/components/UserTable.tsx
@@ -7,18 +7,7 @@ import type { FC } from 'react';
 // ////////////////////////////////////////////////////////////////////////////
 // 3PP Imports
 // ////////////////////////////////////////////////////////////////////////////
-import {
-  Column,
-  Table as ReactTable,
-  useReactTable,
-  getCoreRowModel,
-  getFilteredRowModel,
-  getPaginationRowModel,
-  ColumnDef,
-  flexRender,
-  getSortedRowModel,
-  SortingState,
-} from '@tanstack/react-table'
+import { LiaUserEditSolid } from 'react-icons/lia';
 
 // ////////////////////////////////////////////////////////////////////////////
 // Local Imports
@@ -28,13 +17,16 @@ import type { TUserRole } from '../stores/current-user';
 import { buildQuery } from '../utils/api';
 import { userIsSuperAdmin } from '../utils/auth';
 import { isGuestActive } from '../utils/guest';
-import { titleCase } from '../utils/string';
 import { getTeamName } from '../utils/team';
 
 // ////////////////////////////////////////////////////////////////////////////
 // Styles and CSS
 // ////////////////////////////////////////////////////////////////////////////
+import 'bootstrap/dist/css/bootstrap.css';
+
 import style from '../styles/table.module.scss';
+import type { ColumnDef } from '@tanstack/react-table';
+import { Table, defaultColumnDef } from './Table';
 
 // ////////////////////////////////////////////////////////////////////////////
 // Types and Interfaces
@@ -54,228 +46,14 @@ interface ITeam {
   name: string;
 }
 
-interface ITableProps {
-  data: IUserEntry[];
-  columns: ColumnDef<IUserEntry>[];
-}
-
-// ////////////////////////////////////////////////////////////////////////////
-// Helpers
-// ////////////////////////////////////////////////////////////////////////////
-const defaultColumnDef = <T, >( key: keyof T ): ColumnDef<T> => {
-  return {
-    accessorFn: row => row[key],
-    id: key as string,
-    cell: info => info.getValue(),
-    header: () => <span>{titleCase(key as string)}</span>,
-    footer: props => props.column.id,
-    enableSorting: true,
-  }
-};
+type WithActiveTag<T> = T & { active: boolean; };
 
 // ////////////////////////////////////////////////////////////////////////////
 // Implementation
 // ////////////////////////////////////////////////////////////////////////////
 
-const Filter = ({
-  column,
-  table,
-}: {
-  column: Column<any, any>
-  table: ReactTable<any>
-} ) => {
-  const firstValue = table
-    .getPreFilteredRowModel()
-    .flatRows[0]?.getValue(column.id)
-
-  const columnFilterValue = column.getFilterValue()
-
-  return typeof firstValue === 'number' ? (
-    <div className="flex space-x-2">
-      <input
-        type="number"
-        value={(columnFilterValue as [number, number])?.[0] ?? ''}
-        onChange={e =>
-          column.setFilterValue((old: [number, number]) => [
-            e.target.value,
-            old?.[1],
-          ])
-        }
-        placeholder={`Min`}
-        className="w-24 border shadow rounded"
-      />
-      <input
-        type="number"
-        value={(columnFilterValue as [number, number])?.[1] ?? ''}
-        onChange={e =>
-          column.setFilterValue((old: [number, number]) => [
-            old?.[0],
-            e.target.value,
-          ])
-        }
-        placeholder={`Max`}
-        className="w-24 border shadow rounded"
-      />
-    </div>
-  ) : (
-    <input
-      type="text"
-      value={(columnFilterValue ?? '') as string}
-      onChange={e => column.setFilterValue(e.target.value)}
-      placeholder={`Search...`}
-      className="w-36 border shadow rounded"
-    />
-  )
-}
-
-const PaginationFooter = <T, >( { table }: { table: ReactTable<T> } ) => {
-  return (
-    <div className="flex items-center gap-2">
-        <button
-          className="border rounded p-1"
-          onClick={() => table.setPageIndex(0)}
-          disabled={!table.getCanPreviousPage()}
-        >
-          {'<<'}
-        </button>
-        <button
-          className="border rounded p-1"
-          onClick={() => table.previousPage()}
-          disabled={!table.getCanPreviousPage()}
-        >
-          {'<'}
-        </button>
-        <button
-          className="border rounded p-1"
-          onClick={() => table.nextPage()}
-          disabled={!table.getCanNextPage()}
-        >
-          {'>'}
-        </button>
-        <button
-          className="border rounded p-1"
-          onClick={() => table.setPageIndex(table.getPageCount() - 1)}
-          disabled={!table.getCanNextPage()}
-        >
-          {'>>'}
-        </button>
-        <span className="flex items-center gap-1">
-          <div>Page</div>
-          <strong>
-            {table.getState().pagination.pageIndex + 1} of{' '}
-            {table.getPageCount()}
-          </strong>
-        </span>
-        <span className="flex items-center gap-1">
-          | Go to page:
-          <input
-            type="number"
-            defaultValue={table.getState().pagination.pageIndex + 1}
-            onChange={e => {
-              const page = e.target.value ? Number(e.target.value) - 1 : 0
-              table.setPageIndex(page)
-            }}
-            className="border p-1 rounded w-16"
-          />
-        </span>
-        <select
-          value={table.getState().pagination.pageSize}
-          onChange={e => {
-            table.setPageSize(Number(e.target.value))
-          }}
-        >
-          {[10, 20, 30, 40, 50].map(pageSize => (
-            <option key={pageSize} value={pageSize}>
-              Show {pageSize}
-            </option>
-          ))}
-        </select>
-      </div>
-  );
-}
-
-const Table: FC<ITableProps> = ( { data, columns }: ITableProps ) => {
-  const [sorting, setSorting] = useState<SortingState>([]);
-
-  const table = useReactTable<IUserEntry>({
-    data,
-    columns,
-    state: {
-      sorting,
-    },
-
-    onSortingChange: setSorting,
-    getCoreRowModel: getCoreRowModel(),
-    getFilteredRowModel: getFilteredRowModel(),
-    getPaginationRowModel: getPaginationRowModel(),
-    getSortedRowModel: getSortedRowModel(),
-  });
-
-  return (
-    <div className={ style.container }>
-      <table className={ `${style.table} ${style['user-table']}` }>
-        <thead>
-          {table.getHeaderGroups().map(headerGroup => (
-            <tr key={headerGroup.id}>
-              {headerGroup.headers.map(header => {
-                return (
-                  <th key={header.id} colSpan={header.colSpan}>
-                    {header.isPlaceholder ? null : (
-                      <div
-                      {...{
-                        className: header.column.getCanSort()
-                          ? 'cursor-pointer select-none'
-                          : '',
-                        onClick: header.column.getToggleSortingHandler(),
-                      }}
-                      >
-                        {flexRender(
-                          header.column.columnDef.header,
-                          header.getContext()
-                        )}
-                        {{
-                          asc: ' 🔺',
-                          desc: ' 🔻',
-                        }[header.column.getIsSorted() as string] ?? null}
-                        {header.column.getCanFilter() ? (
-                          <div>
-                            <Filter column={header.column} table={table} />
-                          </div>
-                        ) : null}
-                      </div>
-                    )}
-                  </th>
-                )
-              })}
-            </tr>
-          ))}
-        </thead>
-        <tbody>
-          {table.getRowModel().rows.map(row => {
-            return (
-              <tr key={row.id}>
-                {row.getVisibleCells().map(cell => {
-                  return (
-                    <td key={cell.id}>
-                      {flexRender(
-                        cell.column.columnDef.cell,
-                        cell.getContext()
-                      )}
-                    </td>
-                  )
-                })}
-              </tr>
-            )
-          })}
-        </tbody>
-      </table>
-      { table.getPageCount() > 1 && <PaginationFooter table={table} /> }
-    </div>
-  );
-}
-
 const UserTable: FC = () => {
-  const [users, setUsers] = useState<IUserEntry[]>( [] );
+  const [users, setUsers] = useState<WithActiveTag<IUserEntry>[]>( [] );
   const [teams, setTeams] = useState<ITeam[]>( [] );
 
   useEffect( () => {
@@ -286,7 +64,14 @@ const UserTable: FC = () => {
       const { data } = await response.json();
 
       if ( data ) {
-        setUsers( data );
+        setUsers(
+          data.map( ( user: IUserEntry ) => {
+            return {
+              ...user,
+              active: isGuestActive( user.expiration ),
+            };
+          } )
+        );
       }
     };
 
@@ -306,7 +91,7 @@ const UserTable: FC = () => {
     getTeams();
   }, [] );
 
-  const columns = useMemo<ColumnDef<IUserEntry>[]>(
+  const columns = useMemo<ColumnDef<WithActiveTag<IUserEntry>>[]>(
     () => [
       defaultColumnDef( 'givenName' ),
       defaultColumnDef( 'familyName' ),
@@ -316,10 +101,9 @@ const UserTable: FC = () => {
         cell: info => getTeamName( info.getValue() as string, teams ),
       },
       {
-        ...defaultColumnDef( 'expiration' ),
+        ...defaultColumnDef( 'active' ),
         cell: info => {
-          const exp = info.getValue() as string;
-          const isActive = isGuestActive( exp );
+          const isActive = info.getValue() as boolean;
           return (
             <span className={ style.status }>
               <span className={ isActive ? style.active : style.inactive } />
@@ -328,6 +112,13 @@ const UserTable: FC = () => {
           );
         },
       },
+      {
+        accessorFn: row => row.email,
+        id: "_edit",
+        cell: info => <a href={`/editUser?id=${info.getValue()}`}><LiaUserEditSolid /></a>,
+        header: () => "Edit",
+        enableSorting: false,
+      }
     ],
     [teams]
   );
@@ -336,7 +127,11 @@ const UserTable: FC = () => {
     <div className={ style.container }>
       <Table
         {
-          ...{ data: users, columns }
+          ...{
+            data: users,
+            columns,
+            additionalTableClasses: [ 'user-table' ],
+          }
         }
       />
     </div>
