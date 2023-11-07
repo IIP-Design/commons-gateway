fd504a14f01f5990f52a629f2f5bc2ba142b9cac
mrewers <hello@marek.dev>
2023-11-07 12:25:07 -0500

    feat: generate hashes of new password with previous salts on the client for safer comparison with previous passwords

--- serverless/funcs/password-change/main.go
+++ serverless/funcs/password-change/main.go
@@ -18,9 +18,10 @@ import (
 )
 
 type PasswordReset struct {
-	CurrentPasswordHash string `json:"currentPasswordHash"`
-	NewPassword         string `json:"newPassword"`
-	Email               string `json:"email"`
+	CurrentPasswordHash string   `json:"currentPasswordHash"`
+	NewPassword         string   `json:"newPassword"`
+	HashedPriorSalts    []string `json:"hashesWithPriorSalts"`
+	Email               string   `json:"email"`
 }
 
 func extractBody(body string) (PasswordReset, error) {
@@ -36,15 +37,19 @@ func extractBody(body string) (PasswordReset, error) {
 	return parsed, err
 }
 
+// verifyUser confirms that the user requesting a password change exists
+// and has provided the correct password.
 func verifyUser(parsed PasswordReset) (creds.CredentialsData, error) {
 	var credentials creds.CredentialsData
 
 	_, exists, err := data.CheckForExistingUser(parsed.Email, "guests")
+
 	if err != nil || !exists {
 		return credentials, errors.New("user does not exist")
 	}
 
 	credentials, err = creds.RetrieveCredentials(parsed.Email)
+
 	if err != nil {
 		return credentials, errors.New("failed to load credentials")
 	}
@@ -56,14 +61,14 @@ func verifyUser(parsed PasswordReset) (creds.CredentialsData, error) {
 	return credentials, nil
 }
 
-func checkPasswordReused(email string, newPassword string) (bool, error) {
+func checkPasswordReused(email string, hashedPriorSalts []string) (bool, error) {
 	var err error
 	reused := false
 
 	pool := data.ConnectToDB()
 	defer pool.Close()
 
-	query := "SELECT creation_date, salt, pass_hash FROM password_history WHERE user_id = $1 ORDER BY creation_date DESC LIMIT 24;"
+	query := "SELECT creation_date, pass_hash FROM password_history WHERE user_id = $1 ORDER BY creation_date DESC LIMIT 24;"
 	rows, err := pool.Query(query, email)
 
 	if err != nil {
@@ -71,21 +76,27 @@ func checkPasswordReused(email string, newPassword string) (bool, error) {
 		return reused, err
 	}
 
+	// Convert list of hashes generated using previous salts into a map for easier searching.
+	hashMap := make(map[string]string, len(hashedPriorSalts))
+	for i := range hashedPriorSalts {
+		hashMap[hashedPriorSalts[i]] = hashedPriorSalts[i]
+	}
+
 	defer rows.Close()
 
 	for rows.Next() {
 		var date time.Time
-		var salt string
 		var passHash string
 
-		if err := rows.Scan(&date, &salt, &passHash); err != nil {
+		if err := rows.Scan(&date, &passHash); err != nil {
 			logs.LogError(err, "Pass Reuse Scan Error")
 			return reused, err
 		}
 
-		newPassHash := hashing.GenerateHash(newPassword, salt)
-		if newPassHash == passHash {
-			reused = (newPassHash == passHash)
+		_, found := hashMap[passHash]
+
+		if found {
+			reused = true
 			break
 		}
 	}
@@ -149,13 +160,7 @@ func PasswordChangeHandler(ctx context.Context, event events.APIGatewayProxyRequ
 		return msgs.SendServerError(err)
 	}
 
-	passwordIsSecure := checkNewPasswordStrength(parsed.Email, credentials.Role, parsed.NewPassword)
-
-	if !passwordIsSecure {
-		return msgs.SendCustomError(errors.New("password is not strong enough"), 422)
-	}
-
-	passwordIsReused, err := checkPasswordReused(parsed.Email, parsed.NewPassword)
+	passwordIsReused, err := checkPasswordReused(parsed.Email, parsed.HashedPriorSalts)
 
 	if err != nil {
 		return msgs.SendServerError(err)
@@ -165,6 +170,7 @@ func PasswordChangeHandler(ctx context.Context, event events.APIGatewayProxyRequ
 
 	_, newSalt := hashing.GenerateCredentials()
 	err = updatePassword(parsed.Email, credentials.Salt, parsed.NewPassword, newSalt)
+
 	if err != nil {
 		return msgs.SendServerError(err)
 	}
