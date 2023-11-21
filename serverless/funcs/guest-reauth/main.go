65642060eb3f4edfe223d5f4ea3debb6c06c3f3a
mrewers <hello@marek.dev>
2023-11-21 16:44:55 -0500

    fix: CC20P2-119 force user to update password when an admin resets their password

--- serverless/funcs/guest-reauth/main.go
+++ serverless/funcs/guest-reauth/main.go
@@ -20,6 +20,7 @@ import (
 func guestReauthHandler(ctx context.Context, event events.APIGatewayProxyRequest) (msgs.Response, error) {
 	// Need client role to determine reauthorization logic
 	scope, err := jwt.ExtractClientRole(event.Headers["Authorization"])
+
 	if err != nil {
 		return msgs.SendServerError(err)
 	}
